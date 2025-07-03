592415ce9f6107e6d83478a01806296fad10bbc6
moyo <moriyori10@gmail.com>
2025-07-03 13:50:40 +0900

    models timezone

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.utils import timezon
+from django.utils import timezone
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
